f59eb7dd3ebd77684f0c70c1dd8e7e219bfa6c85
Add00w <jiincade@gmail.com>
2023-06-02 16:06:07 +0300

    Initial Commit

---
+++ FinanceApp/FinanceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceAppApp.swift
+//  FinanceApp
+//
+//  Created by Abdullahi Addow on 6/2/23.
+//
+
+import SwiftUI
+
+@main
+struct FinanceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
